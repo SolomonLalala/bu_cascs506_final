3ce6d970289b48c75976b62fb04f1bb633a525df
Solomon Lalala <solomon_july_2018@outlook.com>
2025-04-27 09:44:53 -0400

    add progress traker and error capture

--- scripts/bootstrap.R
+++ scripts/bootstrap.R
@@ -2,6 +2,11 @@ library(glmnet)
 library(foreach)
 library(doParallel)
 library(tidyverse)
+library(progressr)
+
+# Set up progress bar system globally
+handlers(global = TRUE)
+handlers("txtprogressbar")
 
 train_model_bootstrap <- function(
     X_train, y_train, alpha, n_cores, iteration, lambda) {
@@ -10,29 +15,42 @@ train_model_bootstrap <- function(
   
   set.seed(123)
   
-  bootstrap_results <- foreach(
-    # iterate 500 times, combine results, use glmnet
-    i = 1:iteration, .combine = rbind, .packages = "glmnet" 
-  ) %dopar% { 
-    # expression to evaluate in parallel
+  with_progress({
+    p <- progressor(steps = iteration)
     
-    # random sampling with replacement, same sample size as original train
-    boot_indices <- sample(1:nrow(X_train), replace = TRUE)
-    X_boot <- X_train[boot_indices, ]
-    y_boot <- y_train[boot_indices]
-    
-    # fit model to each bootstrap
-    model_boot <- glmnet(
-      X_boot, y_boot, alpha = alpha, lambda = lambda, standardize = TRUE)
-    
-    # extract features with non-zero coefficients
-    coef_boot <- coef(model_boot)
-    nonzero_cpgs <- rownames(coef_boot)[which(coef_boot != 0)]
-    nonzero_cpgs <- nonzero_cpgs[nonzero_cpgs != "(Intercept)"]
-    
-    # create a data frame with the results
-    data.frame(CpG = nonzero_cpgs, Bootstrap = i)
-  }
+    bootstrap_results <- foreach(
+      # iterate 500 times, combine results, use glmnet
+      i = 1:iteration, .combine = rbind, .packages = "glmnet" 
+    ) %dopar% { 
+      # expression to evaluate in parallel
+      
+      # Update progress at every iteration
+      p()  
+      
+      tryCatch({
+      # random sampling with replacement, same sample size as original train
+      boot_indices <- sample(1:nrow(X_train), replace = TRUE)
+      X_boot <- X_train[boot_indices, ]
+      y_boot <- y_train[boot_indices]
+      
+      # fit model to each bootstrap
+      model_boot <- glmnet(
+        X_boot, y_boot, alpha = alpha, lambda = lambda, standardize = TRUE)
+      
+      # extract features with non-zero coefficients
+      coef_boot <- coef(model_boot)
+      nonzero_cpgs <- rownames(coef_boot)[which(coef_boot != 0)]
+      nonzero_cpgs <- nonzero_cpgs[nonzero_cpgs != "(Intercept)"]
+      
+      # create a data frame with the results
+      data.frame(CpG = nonzero_cpgs, Bootstrap = i)
+    }, error = function(e) {
+      # If error happens, return empty dataframe
+      data.frame(CpG = NA, Bootstrap = i)
+        
+      }) # end of tryCatch
+    } # end of foreach loop
+  }) # end of with_progress block
   
   # save results
   saveRDS(bootstrap_results, "data/bootstrap_results.rds")
